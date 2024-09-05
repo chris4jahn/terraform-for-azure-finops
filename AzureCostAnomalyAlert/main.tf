43af5fec56e78889f7f790163c4699d9fa63ad5e
Christian Forjahn <16921366+chris4jahn@users.noreply.github.com>
2024-09-05 10:40:42 +0200

    Update main.tf

--- AzureCostAnomalyAlert/main.tf
+++ AzureCostAnomalyAlert/main.tf
@@ -1,19 +1,3 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_cost_anomaly_alert" "example" {
   name            = "myalertname"
   display_name    = "My Cost Anomaly Alert"
